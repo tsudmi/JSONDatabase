ebb59fff425d42d80a0d7f4aae4facb08a50bd38
Dmitri Tsumak <tsumak.dmitri@gmail.com>
2015-06-16 01:33:08 +0300

    Add setup.py, modify .gitignore and README files

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # JSON Database
 Automata, Languages and Compilers course project
+
+## Requirements
+* Python 2.7+
+
+## Development Environment Setup
+1. Clone this repo:
+```
+    $ git clone git@github.com:tsudmi/json-database.git
+```    
+2. Create virtualenv:
+```
+    $ cd json-database
+    $ virtualenv venv
+```
+
+3. Download ANTLR4 tool from [here](http://www.antlr.org/download.html).
+
+4. Install JSON Database in development mode::
+```
+    $ venv/bin/python setup.py develop
+```
---
+++ setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+install_requires = [
+    'antlr4-python2-runtime==4.5'
+]
+
+
+setup(
+    name='json-database',
+    version='0.1.0',
+    author='Dmitri Chumak',
+    author_email='tsudmi@ut.ee',
+    url='https://github.com/tsudmi/json-database',
+    description='JSON Database is database which holds data in JSON format.',
+    long_description=open('README.md').read(),
+    packages=find_packages(),
+    install_requires=install_requires,
+    entry_points={
+        'console_scripts': ['json-database=json_database.command_line:main'],
+    },
+    classifiers=[
+        'Intended Audience :: Education',
+        'Intended Audience :: Information Technology',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
